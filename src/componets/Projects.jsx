a1e7cf12ec18adb27ded337a6dc0855cef0c8d12
Matthew Studzinski <88520969+MattStudzinski@users.noreply.github.com>
2024-07-13 23:52:10 -0700

    added react tilt

--- src/componets/Projects.jsx
+++ src/componets/Projects.jsx
@@ -1,50 +1,57 @@
 import React from 'react';
+import {Tilt} from 'react-tilt';
+
 
 const Projects = () => {
     return (
         <section className='projects'>
+
+            
             <div className='projects__card-container'>
 
 
-
-                <div className='projects__card'>
+            <Tilt className="Tilt" options= {{ max: 10, scale: 1.03}}>
+                <article className='projects__card'>
                     <div className='projects__header-container'>
                     <h1 className='projects__title'>Inventory Turner</h1>
                         <h2 className='projects__year'>2023</h2>
                     </div>
                     <ul className='projects__tech-stack'>
-                        <li className='projects__tech'>CSS</li>
-                        <li className='projects__tech'>React</li>
-                        <li className='projects__tech'>MongoDB</li>
+                        <li className='projects__tech' id='tech-css'>CSS</li>
+                        <li className='projects__tech' id='tech-react'>React</li>
+                        <li className='projects__tech' id='tech-mongodb'>MongoDB</li>
                     </ul>
                     <div className='projects__description'>This is where the project description goes, lets keep it to a few lines and i am going to type out a few lines here to act as an example for future descriptions so lets be careful about how much we share!</div>
-                    <img className='projects__image' src='https://images.pexels.com/photos/965345/pexels-photo-965345.jpeg?cs=srgb&dl=pexels-markusspiske-965345.jpg&fm=jpg' alt='Project'/>
+                    <img className='projects__image' src='https://bloggingwizard.com/wp-content/uploads/2021/06/envato-elements-Best-Stock-Photo-Sites.png' alt='Project'/>
                     <div className='projects__button-container'>
                         <a href='#' className='projects__live-button'>View Deployed</a>
                         <a href='#' className='projects__code-button'>Github</a>
                     </div>
-                </div>
+                </article>
+            </Tilt>
 
 
-
-                <div className='projects__card'>
+            <Tilt className="Tilt" options= {{ max: 10, scale: 1.03}}>
+                <article className='projects__card'>
                     <div className='projects__header-container'>
                     <h1 className='projects__title'>Inventory Turner</h1>
                         <h2 className='projects__year'>2023</h2>
                     </div>
-                        <ul className='projects__tech-stack'>
-                            <li className='projects__tech'>CSS</li>
-                            <li className='projects__tech'>React</li>
-                            <li className='projects__tech'>MongoDB</li>
-                        </ul>
-                    <img className='projects__image' src='https://images.pexels.com/photos/965345/pexels-photo-965345.jpeg?cs=srgb&dl=pexels-markusspiske-965345.jpg&fm=jpg' alt='Project'/>
+                    <ul className='projects__tech-stack'>
+                        <li className='projects__tech' id='tech-css'>CSS</li>
+                        <li className='projects__tech' id='tech-react'>React</li>
+                        <li className='projects__tech' id='tech-mongodb'>MongoDB</li>
+                    </ul>
+                    <div className='projects__description'>This is where the project description goes, lets keep it to a few lines and i am going to type out a few lines here to act as an example for future descriptions so lets be careful about how much we share!</div>
+                    <img className='projects__image' src='https://bloggingwizard.com/wp-content/uploads/2021/06/envato-elements-Best-Stock-Photo-Sites.png' alt='Project'/>
                     <div className='projects__button-container'>
                         <a href='#' className='projects__live-button'>View Deployed</a>
                         <a href='#' className='projects__code-button'>Github</a>
                     </div>
 
 
-                </div>
+                </article>
+                </Tilt>
             </div>
 
 
@@ -52,42 +59,45 @@ const Projects = () => {
             <div className='projects__card-container2'>
 
 
-
-                <div className='projects__card'>
+            <Tilt className="Tilt" options= {{ max: 10, scale: 1.03}}>
+                <article className='projects__card'>
                     <div className='projects__header-container'>
                     <h1 className='projects__title'>Inventory Turner</h1>
                         <h2 className='projects__year'>2023</h2>
                     </div>
-                        <ul className='projects__tech-stack'>
-                            <li className='projects__tech'>CSS</li>
-                            <li className='projects__tech'>React</li>
-                            <li className='projects__tech'>MongoDB</li>
-                        </ul>
-                    <img className='projects__image' src='https://images.pexels.com/photos/965345/pexels-photo-965345.jpeg?cs=srgb&dl=pexels-markusspiske-965345.jpg&fm=jpg' alt='Project'/>
+                    <ul className='projects__tech-stack'>
+                        <li className='projects__tech' id='tech-css'>CSS</li>
+                        <li className='projects__tech' id='tech-react'>React</li>
+                        <li className='projects__tech' id='tech-mongodb'>MongoDB</li>
+                    </ul>
+                    <div className='projects__description'>This is where the project description goes, lets keep it to a few lines and i am going to type out a few lines here to act as an example for future descriptions so lets be careful about how much we share!</div>
+                    <img className='projects__image' src='https://bloggingwizard.com/wp-content/uploads/2021/06/envato-elements-Best-Stock-Photo-Sites.png' alt='Project'/>
                     <div className='projects__button-container'>
                         <a href='#' className='projects__live-button'>View Deployed</a>
                         <a href='#' className='projects__code-button'>Github</a>
                     </div>
-                </div>
+                </article>
+            </Tilt>
 
-
-
-                <div className='projects__card'>
+            <Tilt className="Tilt" options= {{ max: 10, scale: 1.03}}>
+                <article className='projects__card'>
                     <div className='projects__header-container'>
                     <h1 className='projects__title'>Inventory Turner</h1>
                         <h2 className='projects__year'>2023</h2>
                     </div>
-                        <ul className='projects__tech-stack'>
-                            <li className='projects__tech'>CSS</li>
-                            <li className='projects__tech'>React</li>
-                            <li className='projects__tech'>MongoDB</li>
-                        </ul>
-                    <img className='projects__image' src='https://images.pexels.com/photos/965345/pexels-photo-965345.jpeg?cs=srgb&dl=pexels-markusspiske-965345.jpg&fm=jpg' alt='Project'/>
+                    <ul className='projects__tech-stack'>
+                        <li className='projects__tech' id='tech-css'>CSS</li>
+                        <li className='projects__tech' id='tech-react'>React</li>
+                        <li className='projects__tech' id='tech-mongodb'>MongoDB</li>
+                    </ul>
+                    <div className='projects__description'>This is where the project description goes, lets keep it to a few lines and i am going to type out a few lines here to act as an example for future descriptions so lets be careful about how much we share!</div>
+                    <img className='projects__image' src='https://bloggingwizard.com/wp-content/uploads/2021/06/envato-elements-Best-Stock-Photo-Sites.png' alt='Project'/>
                     <div className='projects__button-container'>
                         <a href='#' className='projects__live-button'>View Deployed</a>
                         <a href='#' className='projects__code-button'>Github</a>
                     </div>
-                </div>
+                </article>
+            </Tilt>
 
 
             </div>
