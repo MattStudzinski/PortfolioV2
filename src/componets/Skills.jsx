d9965c3de2467759eb6c947f63ba36d8715b253a
Matthew Studzinski <88520969+MattStudzinski@users.noreply.github.com>
2024-07-14 23:13:15 -0700

    trying parallax

--- src/componets/Skills.jsx
+++ src/componets/Skills.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
+import { Parallax } from 'react-parallax';
 import Asteroid1 from '../svg/Asteroid1';
-import Css from '../svg/Css';
 import Asteroid2 from '../svg/Asteroid2';
 import Asteroid3 from '../svg/Asteroid3';
 import Asteroid4 from '../svg/Asteroid4';
@@ -8,18 +8,31 @@ import Asteroid5 from '../svg/Asteroid5';
 const Skills = () => {
     return (
         <section className='skills'>
-            <div className='skills-container'>
-                {/* Container for the asteroid and CSS */}
-                <div className='skills-asteroid-container'>
-                    {/* Asteroid SVG */}
-                    <Asteroid1 />
-                    <Asteroid2 />
-                    <Asteroid3 />
-                    <Asteroid4 />
-                    <Asteroid5 />
-
-                    
-                </div>
+            <div className='skills__container'>
+                <Parallax
+                    blur={0}
+                    bgImage=""
+                    bgImageAlt="background"
+                    strength={200}
+                >
+                    <div className='skills__asteroid-container'>
+                        <div className='skills__asteroid'>
+                            <Asteroid1 />
+                        </div>
+                        <div className='skills__asteroid'>
+                            <Asteroid2 />
+                        </div>
+                        <div className='skills__asteroid'>
+                            <Asteroid3 />
+                        </div>
+                        <div className='skills__asteroid'>
+                            <Asteroid4 />
+                        </div>
+                        <div className='skills__asteroid'>
+                            <Asteroid5 />
+                        </div>
+                    </div>
+                </Parallax>
             </div>
         </section>
     );
