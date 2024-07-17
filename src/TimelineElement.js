7777537a16ff72021254b1fd5325dd43f8f0855d
Matthew Studzinski <88520969+MattStudzinski@users.noreply.github.com>
2024-07-16 22:49:44 -0700

    added timeline

---
+++ src/TimelineElement.js
@@ -0,0 +1,40 @@
+let timelineElements = [
+    {
+        id:1,
+        title: "materials Coordinator",
+        location:"The Everett Clinic",
+        description: "did lots of work such as moving boxes ya know",
+        buttonText: "View Projects",
+        date: "May 2020 - August 2020",
+        icon: "work"
+    },
+    {
+        id:2,
+        title: "Techical advisor",
+        location:"Optum",
+        description: "did lots of work such as moving boxes ya know",
+        buttonText: "View Projects",
+        date: "May 2020 - August 2020",
+        icon: "work"
+    },
+    {
+        id:3,
+        title: "Smartsheets developer",
+        location:"Optum",
+        description: "did lots of work such as moving boxes ya know",
+        buttonText: "View Projects",
+        date: "May 2020 - August 2020",
+        icon: "work"
+    },
+    {
+        id:4,
+        title: "developer",
+        location:"Optum",
+        description: "did lots of work such as moving boxes ya know",
+        buttonText: "View Projects",
+        date: "May 2020 - August 2020",
+        icon: "work"
+    }
+]
+
+export default timelineElements
--- src/componets/Experience.jsx
+++ src/componets/Experience.jsx
@@ -1,10 +1,40 @@
 import React from 'react';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import WorkIcon from "../svg/workIcon.png"
+import timelineElements from '../TimelineElement';
+import 'react-vertical-timeline-component/style.min.css';
 
 const Experience = () => {
+
+    let workIconStyles = { background: "#242424", fill: "#ffffff" };
     return (
-            <section className='experience'>
-                EXPERIENCE
-            </section>
+        <section className='experience'>
+        <VerticalTimeline className='experience__timeline'>
+            {timelineElements.map(element => (
+                <VerticalTimelineElement
+                    key={element.id}
+                    className="experience__timeline-element"
+                    contentArrowStyle={{ borderRight: '7px solid #888' }}
+                    contentStyle={{ background: '#444', color: '#fff' }}
+                    date={element.date}
+                    iconStyle={{ background: '#888', color: '#fff' }}
+                    icon={
+                        <img 
+                                src={WorkIcon} 
+                                alt="Work Icon" 
+                                style={{ width: '60%', height: '60%' }} 
+                            />
+                    }
+                  
+                >
+                    <h3 className="experience__timeline-element-title">{element.title}</h3>
+                    <h4 className="experience__timeline-element-subtitle">{element.location}</h4>
+                    <p>{element.description}</p>
+                    <button>{element.buttonText}</button>
+                </VerticalTimelineElement>
+            ))}
+        </VerticalTimeline>
+    </section>
     );
 };
 
