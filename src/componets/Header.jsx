d05105d50bc377d2ba40932468b1153282469f61
Matthew Studzinski <88520969+MattStudzinski@users.noreply.github.com>
2024-07-29 22:36:52 -0700

    stickey nav works

--- src/componets/Header.jsx
+++ src/componets/Header.jsx
@@ -1,12 +1,50 @@
+import { useEffect, useState, useRef } from "react";
 import About from "./About";
 import Navbar from "./Navbar";
+import StickyNav from "./StickyNav"
+
 
 
 const Header = () => {
+
+    const aboutRef = useRef(null)
+    const [showStickyNav, setShowStickeyNav] = useState(false)
+
+    useEffect(() => {
+        const observerOptions = {
+            root: null,
+            rootMargin: '0px',
+            threshold: .2,
+        }
+
+        const handleIntersection = (entries) => {
+            entries.forEach((entry) => {
+                if (!entry.isIntersecting) {
+                    setShowStickeyNav(true)
+                } else {
+                    setShowStickeyNav(false)
+                }
+            })
+        }
+
+        const observer = new IntersectionObserver(handleIntersection, observerOptions)
+        if(aboutRef.current) {
+            observer.observe(aboutRef.current)
+        }
+
+        return () => {
+            if (aboutRef.current) {
+                observer.unobserve(aboutRef.current)
+            }
+        }
+    }, [])
     return (
         <section className='header'>
+            {showStickyNav && <StickyNav />}
             <Navbar />
+            <section id= "about" ref={aboutRef} className="about">
             <About />
+            </section>
         </section>
     );
 };
