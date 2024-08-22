a6fb76cb04947898f82867562afd4a6076948e07
Matthew Studzinski <88520969+MattStudzinski@users.noreply.github.com>
2024-08-21 22:44:46 -0700

    added rough descriptions

--- src/TimelineElement.js
+++ src/TimelineElement.js
@@ -3,28 +3,28 @@ let timelineElements = [
         id:1,
         title: "Techniacl Solutions Developer",
         location:"Optum",
-        description: "did lots of work such as moving boxes ya know",
+        description: "My role as a Solutions Developer requries me to design and implement a variety of web applications that optimize operations and bring value to the department. Key projects: Issue tracker for warehouse staff. Datamanagement and Stock tracking app",
         date: "March 2024 - Present"
     },
     {
         id:2,
         title: "Smartsheets Developer",
         location:"Optum",
-        description: "did lots of work such as moving boxes ya know",
+        description: "In addition to my primary role, I spearhead the Development and management of Smartsheets for my department and our invilvment in the Pacific West region. Key applications include. procurement issue tracking system. Medical lab coat ordering form. recall information management. courier issue handling. regularly engage with high level individuals",
         date: "January 2024 - Present"
     },
     {
         id:3,
         title: "Sharepoint Administrator",
         location:"The Everett Clinic",
-        description: "did lots of work such as moving boxes ya know, did some more stuff and things like thatcrrcrcr drrcr rcrcrc rcrcr",
+        description: "I serve as the Sharepoint Administrator for my department and two external departments. My responsibilities include: redesign and maintenance: led the complete redesign of the dpearmtnes sharepoint page, enhancing the UX/UI experience. cross deparmtent collaboration, content management, process implementation",
         date: "August 2023 - Present"
     },
     {
         id:4,
         title: "Materials Coordinator",
         location:"The Everett Clinic",
-        description: "did lots of work such as moving boxes ya know",
+        description: "I manage the supply chain and inventory for multiple medical facilities, ensuring that patient care areas are consistently stocked with necessary supplies. Coordinate the transportation of medical supplies from the main warehouse to various clinical sites. Update and maintain the item database, adding new products for reorder and troubleshooting any inventory discrepancies. Handle significant monetary transactions daily, managing upwards of $100,000 in inventory during distribution. Played a key role in the transition to a new gas purchase card provider, ensuring a smooth switch and minimal disruption to operations.",
         date: "March 2023 - Presnet"
     }
 ]
