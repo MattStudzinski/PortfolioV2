b6f186f7b810ccdedba8a17edb95c17a36ecd181
Matthew Studzinski <88520969+MattStudzinski@users.noreply.github.com>
2024-09-09 21:47:42 -0700

    added state to sticky nav

--- src/componets/StickyNav.jsx
+++ src/componets/StickyNav.jsx
@@ -1,17 +1,44 @@
-import { useRef} from 'react';
+import { useState } from 'react';
 
 
 
 const StickyNav = ({ show }) => {
-    const navRef = useRef(null)
+    const [activeLink, setActiveLink] = useState('')
 
     
+    const handleClick = (id) => {
+        setActiveLink(id)
+    }
     return (
-        <div ref={navRef} className={`sticky-navigation ${show ? 'visible' : 'hidden'}`}>
-            <a href='#experience' className='sticky-navigation-link'>Experience</a>
-            <a href='#skills' className='sticky-navigation-link'>Skills</a>
-            <a href='#projects' className='sticky-navigation-link'>Projects</a>
-            <a href='#contact' className='sticky-navigation-link'>Contact</a>
+        <div className={`sticky-navigation ${show ? 'visible' : 'hidden'}`}>
+            <a 
+            href='#experience' 
+            className={`sticky-navigation-link ${activeLink === 'experience' ? 'active' : ''}`}
+            onClick={() => handleClick('experience')}
+            >
+                Experience
+            </a>
+            <a
+            href='#skills' 
+            className={`sticky-navigation-link ${activeLink === 'skills' ? 'active' : ''}`}
+            onClick={() => handleClick('skills')}
+            >
+                Skills
+            </a>
+            <a
+            href='#projects' 
+            className={`sticky-navigation-link ${activeLink === 'projects' ? 'active' : ''}`}
+            onClick={() => handleClick('projects')}
+            >
+                Projects
+            </a>
+            <a
+            href='#contact' 
+            className={`sticky-navigation-link ${activeLink === 'contact' ? 'active' : ''}`}
+            onClick={() => handleClick('contact')}
+            >
+                Contact
+            </a>
         </div>
     );
 };
